df17030de9529e589a459fc2e00ce58b83702324
Andres <andresinsuastyd10@gmail.com>
2019-09-24 18:24:59 -0500

    Cifrando datos con MD5

--- src/main/java/services/BusinessService.java
+++ src/main/java/services/BusinessService.java
@@ -1,7 +1,7 @@
 package services;
 
 import org.springframework.stereotype.Service;
-
+import org.apache.commons.codec.digest.DigestUtils;
 import models.User;
 
 @Service
@@ -9,9 +9,10 @@ public class BusinessService {
 
 	public User usuario() {
 		User user = new User();
+		 
 		user.setId((long) 1);
-		user.setUsername("Andres");
-		user.setPassword("admin123");
+		user.setUsername(DigestUtils.md5Hex("Andres"));
+		user.setPassword(DigestUtils.md5Hex("admin123"));
 		return user;
 	}
 }
